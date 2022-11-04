54d877489195e2a8d45510935365620075900cd4
jzoudavy <jzoudavy@gmail.com>
2022-11-03 22:08:03 -0400

    adl

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-from Tools.scripts.dutree import display
 from selenium import webdriver
 from selenium.common import ElementClickInterceptedException, NoSuchElementException
 
@@ -8,6 +7,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+
+import sqlite3 as sl
 
 url = 'https://www.centris.ca/en/properties~for-sale~brossard?view=Thumbnail'
 
@@ -18,9 +19,10 @@ def scrap_pages(driver):
     if listings[-1].text.split('/n')[0] == '': del listings[-1]
 
     for listing in listings:
-        price=listing.find_element(By.XPATH, ".//div[@class='price']/meta[@itemprop='price']").text
+        price = listing.find_element(By.XPATH, ".//div[@class='price']/meta[@itemprop='price']").text
         mls = listing.find_element(By.XPATH, ".//div[@id='MlsNumberNoStealth']/p").text
-        prop_type = listing.find_element(By.XPATH, ".//div[@class='location-container']/span[@itemprop='category']").text
+        prop_type = listing.find_element(By.XPATH,
+                                         ".//div[@class='location-container']/span[@itemprop='category']").text
         addr = listing.find_element(By.XPATH, ".//div[@class='location-container']/span[@class='address']").text
         city = addr.split('\n')[1]
         sector = addr.split('\n')[2]
@@ -32,7 +34,7 @@ def scrap_pages(driver):
             bathrooms = listing.find_element(By.XPATH, ".//div[@class='sdb']").text
 
         listing_item = {
-            'mls':mls,
+            'mls': mls,
             'price': price,
             'Address': addr,
             'property Type': prop_type,
@@ -45,45 +47,41 @@ def scrap_pages(driver):
 
         centris_list.append(listing_item)
 
+
 if __name__ == '__main__':
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
-    #chrome_options.add_argument("headless")
+    # chrome_options.add_argument("headless")
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
+    centris_list = []
 
-    centris_list=[]
+    driver.get(url)
 
-    driver.get(url) 
-
-    total_pages = driver.find_element(By.CLASS_NAME,'pager-current').text.split('/')[1].strip() 
-    
-    for i in range(1,int(total_pages)):
+    total_pages = driver.find_element(By.CLASS_NAME, 'pager-current').text.split('/')[1].strip()
+    # scrap
+    for i in range(1, int(total_pages)):
 
         try:
             scrap_pages(driver)
             driver.find_element(By.CSS_SELECTOR, 'li.next> a').click()
-            time.sleep(0.8)
+            time.sleep(0.5)
         except ElementClickInterceptedException as initial_error:
             try:
                 if len(driver.find_elements(By.XPATH, ".//div[@class='DialogInsightLightBoxCloseButton']")) > 0:
                     driver.find_element(By.XPATH, ".//div[@class='DialogInsightLightBoxCloseButton']").click()
-                    time.sleep(0.6)
+                    time.sleep(0.5)
                 print('pop-up closed')
                 scrap_pages(driver)
                 driver.find_element(By.CSS_SELECTOR, 'li.next> a').click()
-                time.sleep(0.8)
+                time.sleep(0.5)
             except NoSuchElementException:
                 raise initial_error
-
-
     df = pd.DataFrame(centris_list)
 
-    print(df)
 
-
-
-
-
-
+    # storing into sql
+    con = sl.connect('my-test.db')
+    with con:
+        df.to_sql('mls', con)
